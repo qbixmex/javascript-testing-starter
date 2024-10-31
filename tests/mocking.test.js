6d9b4cbb701a031c287189ac12e68c6a82190842
Daniel González Briseño <qbixmex@gmail.com>
2024-10-30 19:12:19 -0700

    Mocking Time

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,4 +1,4 @@
-import { vi, it, expect, describe, beforeEach } from 'vitest';
+import { vi, it, expect, describe } from 'vitest';
 import {
   getPriceInCurrency,
   getShippingInfo,
@@ -6,6 +6,7 @@ import {
   submitOrder,
   signUp,
   login,
+  isOnline,
 } from '../src/mocking';
 import { getExchangeRate } from '../src/libs/currency';
 import { getShippingQuote } from '../src/libs/shipping';
@@ -211,5 +212,23 @@ describe('mocking', () => {
       expect(sendEmail).toHaveBeenCalledOnce();
     });
   });
+
+  describe('isOnline', () => {
+    it('should return false if current hour is outside opening hours ', () => {
+      vi.setSystemTime('2024-01-01 07:59');
+      expect(isOnline()).toBe(false);
+
+      vi.setSystemTime('2024-01-01 20:01');
+      expect(isOnline()).toBe(false);
+    });
+
+    it('should return true if current hour is within opening hours ', () => {
+      vi.setSystemTime('2024-01-01 08:01');
+      expect(isOnline()).toBe(true);
+
+      vi.setSystemTime('2024-01-01 19:59');
+      expect(isOnline()).toBe(true);
+    });
+  })
 
 });
